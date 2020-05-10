29b4865e9b15474c7575d76bea6dd4718767bf4c
selmi-karim <selmi.kerim@gmail.com>
2020-05-10 23:33:52 +0100

    build: debug apk

---
+++ androidBuild.sh
@@ -0,0 +1,4 @@
+
+npm i
+mkdir -p android/app/src/main/assets
+npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res && cd android && ./gradlew clean assembleDebug
